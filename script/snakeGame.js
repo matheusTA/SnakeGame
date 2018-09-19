cd8acb600d539b323e535ac98dc93c35b5f2ea75
Matheus Antonino <matrajano12@gmail.com>
2018-09-19 02:08:45 -0300

    Terminando parte logica do jogo.

---
+++ script/snakeGame.js
@@ -0,0 +1,94 @@
+window.onload = function(){
+ 
+    var stage = document.getElementById('stage');
+    var context = stage.getContext("2d");
+    document.addEventListener("keydown", keyPush);
+    setInterval(game, 80);
+
+    const velocidade = 1;
+
+    var velocidadeX = 0;
+    var velocidadeY = 0;
+    var pontoX = 10;
+    var pontoY = 15;
+    var tamanhoTabuleiro = 30;
+    var qntPecas = 20;
+    var posicaoMacaX = 15; 
+    var posicaoMacaY = 15;
+
+    var rastro = [];
+    var tail = 5;
+
+    function game(){
+        pontoX += velocidadeX;
+        pontoY += velocidadeY;
+        if (pontoX < 0) {
+            pontoX = qntPecas-1;
+        }
+        if (pontoX > qntPecas-1) {
+            pontoX = 0;
+        }
+        if (pontoY < 0) {
+            pontoY = qntPecas-1;
+        }
+        if (pontoY > qntPecas-1) {
+            pontoY = 0;
+        }
+
+        context.fillStyle = "black";
+        context.fillRect(0,0, stage.width, stage.height);
+
+        context.fillStyle = "red";
+        context.fillRect(posicaoMacaX*tamanhoTabuleiro, posicaoMacaY*tamanhoTabuleiro, tamanhoTabuleiro,tamanhoTabuleiro);
+
+        context.fillStyle = "gray";
+        for (var i = 0; i < rastro.length; i++) {
+            context.fillRect(rastro[i].x*tamanhoTabuleiro, rastro[i].y*tamanhoTabuleiro, tamanhoTabuleiro-1,tamanhoTabuleiro-1);
+            if (rastro[i].x == pontoX && rastro[i].y == pontoY)
+            {
+                velocidadeX = velocidadeY=0;
+                tail = 5;
+            }
+        }
+
+        rastro.push({x:pontoX,y:pontoY })
+        while (rastro.length > tail) {
+            rastro.shift();
+        }
+
+        if (posicaoMacaX == pontoX && posicaoMacaY == pontoY){
+            tail++;
+            posicaoMacaX = Math.floor(Math.random()*qntPecas);
+            posicaoMacaY = Math.floor(Math.random()*qntPecas);
+        }
+
+    }
+
+    function keyPush(event){
+
+        switch (event.keyCode) {
+            case 37: // Left
+                velocidadeX = -velocidade;
+                velocidadeY = 0;
+                break;
+            case 38: // up
+                velocidadeX = 0;
+                velocidadeY = -velocidade;
+                break;
+            case 39: // right
+                velocidadeX = velocidade;
+                velocidadeY = 0;
+                break;
+            case 40: // down
+                velocidadeX = 0;
+                velocidadeY = velocidade;
+                break;         
+            default:
+               
+                break;
+        }
+
+
+    }
+
+}
